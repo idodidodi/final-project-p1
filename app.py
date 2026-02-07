fc710b9f17a246f64514596192a605461af4db1d
Ido Ayal <idodidodi@gmail.com>
2026-02-07 16:15:12 +0200

    feat: Import `os` module and display environment variables on the `/hello` route.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,9 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/hello')
+@app.route("/hello")
 def hello():
-    return "Hello World"
-
+    return f"""
+    ENV: {os.environ.get('APP_ENV')}<br>
+    MESSAGE: {os.environ.get('APP_MESSAGE')}<br>
+    PASSWORD: {os.environ.get('DB_PASSWORD')}
+    """
+    
 if __name__ == '__main__':
     app.run(debug=True)
